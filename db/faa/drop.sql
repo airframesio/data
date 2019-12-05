eceb0c74eaf017c6aaf86900b3cd6e0c72f3cb0b
Kevin Elliott <kevin@welikeinc.com>
2019-12-04 19:12:27 -0800

    Add the FAA db schema

---
+++ db/faa/drop.sql
@@ -0,0 +1,6 @@
+
+DROP TABLE IF EXISTS faa_reserved_n_numbers;
+DROP TABLE IF EXISTS faa_engine_reference;
+DROP TABLE IF EXISTS faa_aircraft_reference;
+DROP TABLE IF EXISTS faa_aircraft_deregistered;
+DROP TABLE IF EXISTS faa_aircraft_registration_master;
