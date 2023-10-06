8246dbfb286035c14fa2139fd3f12a4c0fc41a38
Kevin Elliott <kevin@welikeinc.com>
2023-10-05 18:15:57 -0700

    Removed outdated SQL stuff

--- db/faa/drop.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX idx_faa_arm_mode_s_code_hex;
-DROP INDEX idx_faa_arm_n_number;
-
-DROP TABLE IF EXISTS faa_reserved_n_numbers;
-DROP TABLE IF EXISTS faa_engine_reference;
-DROP TABLE IF EXISTS faa_aircraft_reference;
-DROP TABLE IF EXISTS faa_aircraft_deregistered;
-DROP TABLE IF EXISTS faa_aircraft_registration_master;
